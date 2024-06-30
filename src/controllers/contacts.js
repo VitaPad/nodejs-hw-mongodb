d6c4a1ce15ae48a80374dda7b54d618b2e35b1b3
Vita <v.mars22@ukr.net>
2024-07-01 00:31:21 +0300

    changes patch

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -55,7 +55,7 @@ export const patchContactController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: contact,
+    data: contact.data,
   });
 };
 
