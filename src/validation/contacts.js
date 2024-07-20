dbfe9930513ff7c457d49c928a9127b5fc3dcf8d
Vita <v.mars22@ukr.net>
2024-07-20 23:03:43 +0300

    changes joi chema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -19,7 +19,6 @@ export const createUpdateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string()
     .pattern(/^\+?[0-9]{3,20}$/)
-    .required()
     .messages({
       'string.pattern.base':
         '"phoneNumber" must be a valid phone number with 3 to 20 digits',
